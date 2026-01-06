acec2da3542877d3cad7be9d2a846802e5043a30
Craticism <130575553+Craticism@users.noreply.github.com>
2026-01-07 02:26:32 +0800

    hapus file lanyard

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import Lanyard from "./components/Lanyard";
 import Cv from "./components/Cv";
 
 function App() {
@@ -13,9 +12,8 @@ function App() {
       <Home />
       <About />
       <Projects />
-      <Cv />
-      <Lanyard />
       <Contact />
+      <Cv />
     </>
   );
 }
